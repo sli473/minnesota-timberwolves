b91724a9c5dc87a90c26ee7a0cbd7da634ff8699
Samule Li <samuleli.1996@gmail.com>
2018-08-29 01:14:29 +1200

    setting up Timberwolves project

---
+++ README.md
@@ -0,0 +1,1 @@
+Timberwolves fan page
---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { stats, data } from 'nba.js';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: null,
+      pic: null
+    };
+  }
+  componentWillMount = async () => {
+    const url =
+      'http://stats.nba.com/stats/commonallplayers?IsOnlyCurrentSeason=0&LeagueID=00&Season=2017-18'; // site that doesn’t send Access-Control-*
+    const incomingStats = await fetch(url); // https://cors-anywhere.herokuapp.com/https://example.com
+    const statPromise = await incomingStats.json();
+    const statsData = statPromise.resultSets[0].rowSet;
+    const filteredPlayers = statsData.filter(player => player[10] == 'MIN');
+
+    const playerStats = await this.fetchPlayerStats(filteredPlayers);
+
+    this.setState({ players: playerStats });
+  };
+
+  fetchPlayerStats = async players => {
+    const playerStats = [];
+    console.log(players);
+    await Promise.all(
+      players.map(async player => {
+        const playerName = player[1].split(', ');
+        const playerStatsPromise = await fetch(
+          `https://nba-players.herokuapp.com/players-stats/${playerName[0]}/${playerName[1]}`
+        );
+        console.log(playerStatsPromise);
+        const contentType = playerStatsPromise.headers.get('content-type');
+        if (contentType && contentType.indexOf('application/json') !== -1) {
+          const playerStat = await playerStatsPromise.json();
+          console.log(playerStat ? playerStat : 'I fucked it up', playerName);
+          const playerImagePromise = await fetch(
+            `https://nba-players.herokuapp.com/players/${playerName[0]}/${playerName[1]}`
+          );
+          const playerImage = await playerImagePromise.blob();
+          const imageURL = URL.createObjectURL(playerImage);
+          playerStats.push({
+            ...playerStat,
+            imageURL
+          });
+        }
+      })
+    );
+
+    console.log('what these boys look like g', playerStats);
+    return playerStats;
+  };
+
+  render() {
+    const { players, pic } = this.state;
+    return (
+      <div>
+        oh shit allPlayers
+        {players ? (
+          players.map(player => (
+            <div key={player.name}>
+              {player.name}
+              <img height={100} src={player.imageURL} />
+            </div>
+          ))
+        ) : (
+          <div> Loading... </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
